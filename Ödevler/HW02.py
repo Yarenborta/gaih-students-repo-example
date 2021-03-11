ffd2ab160ff1167beb6db3512d93aec2019c1687
yarenborta <53098018+Yarenborta@users.noreply.github.com>
2021-03-11 15:04:13 +0300

    Create HW02.py

---
+++ Ödevler/HW02.py
@@ -0,0 +1,23 @@
+
+# HOMEWORK-2
+# 1.CV application.
+# 2.Create 5 dictionaries. Each dictionary should represent a CV.
+# 3.Create a CV containing the information of the 5 created people.
+# 4.Print the information on CVs created on the screen.
+
+
+mulakat = {"CV_1 :":["AD: Fatma","SOYAD: BÖRTA","DOĞUM TARİHİ: 13.06.2002","Yaş:18","BÖLÜM:Makine Mühendisliği","Başarılı Olunan Alanlar:Python Programlama Dili"],"CV_2 :":["AD:Nur","SOYAD: IŞIK","DOĞUM TARİHİ: 11.08.1995 ","Yaş:26","BÖLÜM: Mütercim Tercümanlığı","Başarılı OLunan Alanlar: Yabancı Dil"],"CV_3 :":["AD:Ela","SOYAD: YERLİKAYA","YAŞ:22","DOĞUM TARİHİ:02.08.1999","BÖLÜM:Elektrik Elektronik Mühendisliği","Başarılı Olunan Alanlar: AutoCad programlama dili"],"CV_4 :":["AD: Egemen","SOYAD:TÜRKOĞLU","DOĞUM TARİHİ:17.05.1998","YAŞ:23","BÖLÜM:Mimarlık","Başarılı Olunan Alanlar: Çizim"],"CV_5 :":["AD:Oğuz","SOYAD:YÜREKTEN","DOĞUM TARİHİ:22.10.1997","YAŞ:24","BÖLÜM: Doktorluk","Başarılı OLunan Alanlar: Yüzme Şampiyonluğu"]}
+for m in mulakat.items():
+    print(m)
+
+
+####### If we want to choose the mulakat ourselves;
+
+#isim = input("Lütfen istediğiniz CV'yi giriniz: ")
+#print("{}'in CV'si: ".format(isim))
+#for m in (mulakat[isim]):
+    #print(m)
+
+
+  
+  
